aa28a0ea43028972ae218aa91fa220786262639b
Azhar Khan <azmanian.devil@gmail.com>
2014-09-13 21:04:27 -0400

    songs loading from db, some cleanup

--- listeningto/models.py
+++ listeningto/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Songs(models.Model):
 
+
+class Songs(models.Model):
     track_url = models.CharField(max_length=500, blank=False)
     track_name = models.CharField(max_length=500, blank=False)
     stream_url = models.CharField(max_length=500, blank=False)
     track_id = models.CharField(max_length=500, blank=False)
     track_artwork_url = models.CharField(max_length=500, blank=False)
 
+
 def __str__(self):
-    return self.song_id
+    return self.track_name
