dbe37534f5fdce402ee5bae322895248f4ad7cc9
Jared Ali <jared@wattpad.com>
2014-09-21 16:37:39 -0400

    adding basic auth to project

--- listeningto/views.py
+++ listeningto/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
+from django.contrib import auth
 from listeningto.models import Songs
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -30,3 +31,36 @@ def add_song(request):
 def delete_song(request, pk):
     get_object_or_404(Songs, pk=pk).delete()
     return HttpResponseRedirect(reverse('home'))
+
+
+def login_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        # Correct password, and the user is marked "active"
+        auth.login(request, user)
+        # Redirect to a success page.
+        return HttpResponseRedirect("/account/loggedin/")
+    else:
+        # Show an error page
+        return HttpResponseRedirect("/account/invalid/")
+
+
+def logout_view(request):
+    auth.logout(request)
+    # Redirect to a success page.
+    return HttpResponseRedirect("/account/loggedout/")
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/books/")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {
+        'form': form,
+    })
