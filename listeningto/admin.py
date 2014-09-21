2f44e71bfdfb8bdc87ff5ba7a059685e089fa8de
Azhar Khan <azmanian.devil@gmail.com>
2014-09-21 19:16:17 -0400

    added songs in admin panel

--- listeningto/admin.py
+++ listeningto/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Song
 
 # Register your models here.
+
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('track_name', )
+
+admin.site.register(Song, SongAdmin)
