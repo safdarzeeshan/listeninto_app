0308f53b1b134284f446909b8f97c426be8cb07b
Azhar Khan <azmanian.devil@gmail.com>
2014-12-16 21:53:09 -0500

    added string representation, fixed some spacing

--- listeningto/models.py
+++ listeningto/models.py
@@ -24,11 +24,15 @@ class Song(models.Model):
     def __str__(self):
         return self.track_name
 
+
 class UserPlaylist(models.Model):
-    
     playlist = models.ForeignKey(Playlist)
     song = models.ForeignKey(Song)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} on {}'s list".format(self.song.track_name, self.playlist.user.username)
+
 
 class Recommendation(models.Model):
     receipient = models.ForeignKey(User)
@@ -37,6 +41,7 @@ class Recommendation(models.Model):
     description = models.CharField(max_length=500, blank=True)
     played = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Comment(models.Model):
     message = models.CharField(max_length=500, null=False, blank=False)
