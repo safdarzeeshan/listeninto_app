57615f6ccc2a21b47480d8d9d237509139ab571f
Zeeshan Safdar <safdarzeeshan@gmail.com>
2016-07-17 19:16:11 -0400

    updat wsgi

--- listeningto_app/wsgi.py
+++ listeningto_app/wsgi.py
@@ -10,5 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "listeningto_app.settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+# import django.core.handlers.wsgi
+# application = django.core.handlers.wsgi.WSGIHandler()
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
